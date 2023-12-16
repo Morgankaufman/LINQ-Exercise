f4d66c628a58eb72e5a4f0aea1c4ae3270891574
Morgan Kaufman <Morgankaufman13@yahoo.com>
2023-12-16 11:11:44 -0500

    finished LINQ exercise 1

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -4,7 +4,25 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<string> Games = new List<string>
+            {
+                "Super Mario Odyssey",
+                "Ratchet and Clank",
+                "The Witcher 3",
+                "Uncharted",
+                "Jak and Daxter",
+                "Spyro",
+                "Red Dead Redemption",
+            };
+
+            var GamesOrdered = Games.OrderBy(game => game.Length);
+
+            Console.WriteLine("List of games ordered by length.");
+
+            foreach (var game in GamesOrdered) 
+            {
+                Console.WriteLine(game);
+            }
         }
     }
 }
